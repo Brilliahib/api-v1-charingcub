b963d6a8d512f95dacc9c05b9f0cfb70b7de7fea
brilliahib <brilliahib21@gmail.com>
2025-05-26 09:07:31 +0700

    feat: get lastest articles functionality

--- routes/api.php
+++ routes/api.php
@@ -175,6 +175,7 @@ Route::middleware('auth:api')->group(function () {
 
 Route::get('/article/{id}', [ArticleController::class, 'show']);
 Route::get('/article', [ArticleController::class, 'getAllArticle']);
+Route::get('/article/latest', [ArticleController::class, 'getLatestArticles']);
 
 Route::get('/article-types', [ArticleTypeController::class, 'getAllArticleType']);
 Route::get('/article-types/{id}', [ArticleTypeController::class, 'showArticleType']);
